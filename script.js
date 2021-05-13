cb18406ce8f96af8d70c76afdf0778859eb35edf
Brandon Clark <clarkwbrandon2@gmail.com>
2021-05-13 11:38:55 -0700

    generated all js variables

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let generate = document.getElementById("generate")
+
+
 document.getElementById('generate').addEventListener('click', () => {
+  let length = prompt("how long do you want your password?")
+  let lowerCase = confirm("lower case?")
+  let upperCase = confirm("upper case?")
+  let nums = confirm("nums?")
+  let specChars = confirm("special characters?")
+  let lowerArray = "abcdefghijklmnopqrstuvwxyz"
+  let upperArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  let numArray = "0123456789"
+  let specArray = "!@#$%^&*()"
+  let chosenArray = ""
+  let password = ""
 
 })
