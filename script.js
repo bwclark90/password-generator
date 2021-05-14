c2efce6ff941d55d3615d5c5cd79638641ff1e93
Brandon Clark <clarkwbrandon2@gmail.com>
2021-05-14 09:50:33 -0700

    Most Javascript written

--- script.js
+++ script.js
@@ -11,7 +11,22 @@ document.getElementById('generate').addEventListener('click', () => {
   let upperArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   let numArray = "0123456789"
   let specArray = "!@#$%^&*()"
-  let chosenArray = ""
-  let password = ""
+  let chosenArray = []
+  let password = chosenArray
+  let count = lowerCase + upperCase + nums + specChars
+
+  if (lowerCase) {chosenArray += lowerArray}
+  if (upperCase) {chosenArray += upperArray}
+  if (nums) { chosenArray += numArray}
+  if (specChars) { chosenArray += specArray}
+
+  
+let passwordGeneration = []
+  for (i = 0; i < length; i++) {
+    passwordGeneration.push(chosenArray [Math.floor(Math.random() * chosenArray.length)])
+  }
+console.log(passwordGeneration)
+  document.getElementById('password').innerHTML = passwordGeneration.join('')
+
 
 })
