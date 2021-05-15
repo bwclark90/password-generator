1cbc740b58e9f3b12a3a3412baf77fd404ecbddc
Brandon Clark <clarkwbrandon2@gmail.com>
2021-05-14 18:11:02 -0700

    finished notes and fixed javascript errors

--- script.js
+++ script.js
@@ -1,32 +1,56 @@
 let generate = document.getElementById("generate")
 
-
+// defining variables // 
 document.getElementById('generate').addEventListener('click', () => {
-  let length = prompt("how long do you want your password?")
+  let length = parseInt(prompt("how long do you want your password?"))
   let lowerCase = confirm("lower case?")
   let upperCase = confirm("upper case?")
-  let nums = confirm("nums?")
+  let nums = confirm("numbers?")
   let specChars = confirm("special characters?")
   let lowerArray = "abcdefghijklmnopqrstuvwxyz"
   let upperArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   let numArray = "0123456789"
   let specArray = "!@#$%^&*()"
   let chosenArray = []
-  let password = chosenArray
+  let passwordGeneration = []
   let count = lowerCase + upperCase + nums + specChars
+  let lowerCon = "b"
+  let upperCon = "C"
+  let specCon = "$"
+  let numCon = "8"
+  let conAmount = 0
 
-  if (lowerCase) {chosenArray += lowerArray}
-  if (upperCase) {chosenArray += upperArray}
-  if (nums) { chosenArray += numArray}
-  if (specChars) { chosenArray += specArray}
+  // if statement for chosen user data
+  if (lowerCase) {chosenArray += lowerArray
+  passwordGeneration.push(lowerCon) 
+conAmount ++}
+
+  if (upperCase) {chosenArray += upperArray
+  passwordGeneration.push(upperCon)
+conAmount ++}
+  if (nums) { chosenArray += numArray
+    passwordGeneration.push(numCon)
+  conAmount ++}
+  if (specChars) { chosenArray += specArray
+    passwordGeneration.push(specCon)
+  conAmount ++}
+  
+// if user does not select a parameter, then alert or redo
+  if (lowerCase === false && upperCase === false && specChars === false && nums === false) {
+    alert("You must choose at least one parameter") 
+    document.getElementById("generate")  }
+      else{
+   
+
+
 
   
-let passwordGeneration = []
-  for (i = 0; i < length; i++) {
+//array selection from random generator 
+  for (i = 0; i < length - conAmount; i++) {
     passwordGeneration.push(chosenArray [Math.floor(Math.random() * chosenArray.length)])
   }
 console.log(passwordGeneration)
   document.getElementById('password').innerHTML = passwordGeneration.join('')
 
 
-})
+  }})
